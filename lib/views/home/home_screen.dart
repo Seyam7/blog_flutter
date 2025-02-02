3d9c79fcb59fb1037bcaadc9bad1d3dd11876366
Sadman Seyam <sadman.seyam07@gmail.com>
2025-02-02 18:29:34 +0600

    at home page when we will swipe down than page will refresh

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:blog_flutter/utils/config.dart';
 import 'package:blog_flutter/views/my_posts/my_posts.dart';
 import 'package:blog_flutter/views/post_create_or_update/post_create_or_update.dart';
@@ -23,56 +25,63 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 0.780,
-          ),
-          itemBuilder: (_,index){
-            return InkWell(
-              onTap: (){
-                Get.to(()=>PostDetailsView());
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.red,
-                      image: DecorationImage(
-                        image: NetworkImage('https://www.atatus.com/blog/content/images/2023/01/significance-of-writing-code.png'),
-                        fit: BoxFit.cover,
+      body: RefreshIndicator(
+        onRefresh: ()async{
+          Timer(Duration(seconds: 3), (){
+            print('Data lopaded');
+          });
+        },
+        child: GridView.builder(
+          padding: EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.780,
+            ),
+            itemBuilder: (_,index){
+              return InkWell(
+                onTap: (){
+                  Get.to(()=>PostDetailsView());
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.red,
+                        image: DecorationImage(
+                          image: NetworkImage('https://www.atatus.com/blog/content/images/2023/01/significance-of-writing-code.png'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5,),
-                  Text(
-                    '10 tips for boosting your code readability',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600
+                    SizedBox(height: 5,),
+                    Text(
+                      '10 tips for boosting your code readability',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 2,),
-                  Text(
-                    'Author: Md. Shirajul Islam',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 12,
-                        color: Colors.black.withOpacity(.5),
+                    SizedBox(height: 2,),
+                    Text(
+                      'Author: Md. Shirajul Islam',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 12,
+                          color: Colors.black.withOpacity(.5),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
+                  ],
+                ),
+              );
+            },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: (){
